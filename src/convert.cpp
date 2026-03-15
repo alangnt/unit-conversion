72f1a6832b4f4faca401858da2e3490b16f7ea95
alangnt <geirnaert.alan@icloud.com>
2026-03-15 20:09:05 +0100

    feat: early return in case quantity is not between 1 and max size of quantities

--- src/convert.cpp
+++ src/convert.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 
 std::string convert(int quantity, double value, std::vector<Quantity>& quantities, bool cgs_to_si) {
+  if (quantity <= 0 || quantity > quantities.size()) return "No value";
+
   double converted_value;
   if (cgs_to_si) converted_value = value / quantities[quantity - 1].conversion_factor;
   else converted_value = value * quantities[quantity - 1].conversion_factor;
